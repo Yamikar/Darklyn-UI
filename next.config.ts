03bfcba284799e44b3a5cfc5c05c91dd825f36df
Yılmaz Keskin <104618545+Yamikar@users.noreply.github.com>
2025-10-08 13:44:13 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // build sırasında ESLint hatalarını yok say
+  },
+  typescript: {
+    ignoreBuildErrors: true, // build sırasında TS hatalarını yok say
+  },
 };
 
 export default nextConfig;
